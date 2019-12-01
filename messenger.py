9c89755ef5f284eb603fcec4f0694cfe7d92770a
Bozhidar <bspetrovv@gmail.com>
2019-12-01 20:15:54 +0200

    Third exercise from first exam prep test.

---
+++ messenger.py
@@ -0,0 +1,41 @@
+import operator
+max = int(input())
+
+records = dict()
+
+while True:
+    command = input()
+    if command == 'Statistics':
+        break
+    tokens = command.split('=')
+    if tokens[0] == 'Add':
+        username = tokens[1]
+        sent = int(tokens[2])
+        received = int(tokens[3])
+        if username not in records:
+            records[username] = [sent, received]
+    elif tokens[0] == 'Message':
+        sender = tokens[1]
+        receiver = tokens[2]
+        if sender in records and receiver in records:
+            records[sender][0] += 1
+            records[receiver][1] += 1
+            if sum(records[sender]) >= max:
+                records.pop(sender)
+                print(f'{sender} reached the capacity!')
+            if sum(records[receiver]) >= max:
+                records.pop(receiver)
+                print(f'{receiver} reached the capacity!')
+    elif tokens[0] == 'Empty':
+        username = tokens[1]
+        if username == 'ALL':
+            records = {}
+        else:
+            if username in records:
+                records.pop(username)
+records = dict(sorted(records.items(), key=operator.itemgetter(0), reverse=True))
+print(f'Users count: {len(records)}')
+for key, value in sorted(records.items()):
+    print(f'{key} - {sum(value)}')
+
+
