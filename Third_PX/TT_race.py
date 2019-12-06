5cf7bc02edfa05eea1a0a5877029109af6a1784b
Bozhidar <bspetrovv@gmail.com>
2019-12-06 16:34:32 +0200

    Function for getting the geohash code length, done!

--- Third_PX/TT_race.py
+++ Third_PX/TT_race.py
@@ -7,6 +7,12 @@ def group_len(encrypted_sentence):
     for i in range(len(encrypted_sentence)):
         if encrypted_sentence[i] == '!':
             main_key = f'{encrypted_sentence[i-2]}{encrypted_sentence[i-1]}'
-            return main_key
+            try:
+                main_key = int(main_key)
+                return main_key
+            except:
+                main_key = int(encrypted_sentence[i-1])
+                return main_key
 
-group_len(encrypted_sentence)
+
+print(group_len(encrypted_sentence))
